a285979e76da3627ce06a236f9820129c40c114b
Oleg Lozovik <o.lozovik@gmail.com>
2021-12-05 14:57:25 +0200

    regexp

--- models/Contact.js
+++ models/Contact.js
@@ -1,11 +1,11 @@
 const { Schema, model } = require('mongoose')
 const Joi = require('joi')
 
-const regEx = {
-  email: new RegExp('^[^@s]+@[^@s]+.[^@s]+$'),
-  phone: new RegExp(
-    '\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}',
-  ),
+const regExp = {
+  name: /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+  email: /^(.+)@(.+)\.(.+)$/,
+  phone:
+    /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
 }
 
 const contactSchema = new Schema(
@@ -13,22 +13,23 @@ const contactSchema = new Schema(
     name: {
       type: String,
       required: [true, 'Set name for contact'],
-      min: 2,
-      max: 50,
+      match: regExp.name,
+      minlength: 2,
+      maxlength: 50,
     },
     email: {
       type: String,
       required: [true, 'Set email for contact'],
-      match: regEx.email,
-      min: 5,
-      max: 50,
+      match: regExp.email,
+      minlength: 5,
+      maxlength: 50,
     },
     phone: {
       type: String,
       required: [true, 'Set phone for contact'],
-      match: regEx.phone,
-      min: 7,
-      max: 20,
+      match: regExp.phone,
+      minlength: 7,
+      maxlength: 20,
     },
     favorite: {
       type: Boolean,
@@ -39,9 +40,9 @@ const contactSchema = new Schema(
 )
 
 const joiContactsSchema = Joi.object({
-  name: Joi.string().min(2).max(50).required(),
-  email: Joi.string().min(5).max(50).required().pattern(regEx.email),
-  phone: Joi.string().min(7).max(20).required().pattern(regEx.phone),
+  name: Joi.string().min(2).max(50).required().pattern(regExp.name),
+  email: Joi.string().min(5).max(50).required().pattern(regExp.email),
+  phone: Joi.string().min(7).max(20).required().pattern(regExp.phone),
   favorite: Joi.bool(),
 })
 
